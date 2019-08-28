496c7752becb279748cfcfb3a1a4bce4228da99f
Ree_ <49682119+Ree-jp@users.noreply.github.com>
2019-08-28 17:18:39 +0900

    Add files via upload

--- src/Ree/elevator/main.php
+++ src/Ree/elevator/main.php
@@ -15,53 +15,36 @@ class main extends PluginBase implements Listener
 {
     const block = 42;
     const elevator = 148;
-    
-    public function onEnable ()
+
+    public function onEnable()
     {
-       $this->getLogger()->info(">>loading now...");
+        $this->getLogger()->info("loading now...");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->elevator = new Config($this->getDataFolder() . "elevator.yml" ,Config::YAML ,array(
+        $this->elevator = new Config($this->getDataFolder() . "elevator.yml", Config::YAML, array(
             'Authority required for use//使用に必要な権限[op or true]' => 'op',
-            'Available world//使用可能なワールド[string]' => 'world,lobby'
-            ));
+            'Available world//使用可能なワールド[string]' => 'world,lobby',
+            'message//メッセージ[true or false]' => 'true'
+        ));
+        $this->authority = $this->elevator->get("Authority required for use//使用に必要な権限[op or true]");
         $this->world = $this->elevator->get("Available world//使用可能なワールド[string]");
-        $this->world = explode("," , $this->world);
+        $this->world = explode(",", $this->world);
+        $this->message = $this->elevator->get("message//メッセージ[true or false]");
     }
 
-    public function onJump (PlayerJumpEvent $ev)
+    private function elevator($level, $x, $y, $z)
     {
-        $p = $ev->getPlayer();
-        $n = $p->getName();
-        $level = $p->getlevel();
-        $x = $p->getX();
-        $y = $p->getY();
-        $z = $p->getZ();
+        $vector3 = new Vector3($x, $y, $z);
 
-        foreach($this->world as $world)
-        {
-            $world = $this->getServer()->getLevelByName($world);
-            if($level == $world)
-            {
-                if($this->elevator($level ,$x ,$y ,$z))
-                {
-                     for($i = 2 ;$i <= 15 ;$i++)
-                     {
-                         if(self::elevator == $level->getBlock(new Vector3($x ,$y + $i ,$z))->getId())
-                         {
-                             if(self::block == $level->getBlock(new Vector3($x ,$y + $i - 1 ,$z))->getId())
-                             {
-                                 $p->teleport(new Vector3($x ,$y + $i ,$z));
-                                 $p->sendMessage("エレベーターを使用して".$i."ブロック上がりました");
-                                 break;
-                             }
-                         }
-                     }
-                }
+        if (self::elevator == $level->getBlock(new Vector3($x, $y, $z))->getId()) {
+            if (self::block == $level->getBlock(new Vector3($x, $y - 1, $z))->getId()) {
+                return true;
             }
         }
+
+        return false;
     }
 
-    public function onSneak (PlayerToggleSneakEvent $ev)
+    public function onJump(PlayerJumpEvent $ev)
     {
         $p = $ev->getPlayer();
         $n = $p->getName();
@@ -70,22 +53,20 @@ class main extends PluginBase implements Listener
         $y = $p->getY();
         $z = $p->getZ();
 
-        foreach($this->world as $world)
-        {
-            $world = $this->getServer()->getLevelByName($world);
-            if($level == $world)
-            {
-                if($this->elevator($level ,$x ,$y ,$z))
-                {
-                    for ($i = 2; $i <= 15; $i++)
-                    {
-                        if (self::elevator == $level->getBlock(new Vector3($x, $y - $i, $z))->getId())
-                        {
-                            if (self::block == $level->getBlock(new Vector3($x, $y - $i - 1, $z))->getId())
-                            {
-                                $p->teleport(new Vector3($x, $y - $i, $z));
-                                $p->sendMessage("エレベーターを使用して" . $i . "ブロック下りました");
-                                break;
+        if($this->request($p)) {
+            foreach ($this->world as $world) {
+                $world = $this->getServer()->getLevelByName($world);
+                if ($level == $world) {
+                    if ($this->elevator($level, $x, $y, $z)) {
+                        for ($i = 2; $i <= 15; $i++) {
+                            if (self::elevator == $level->getBlock(new Vector3($x, $y + $i, $z))->getId()) {
+                                if (self::block == $level->getBlock(new Vector3($x, $y + $i - 1, $z))->getId()) {
+                                    $p->teleport(new Vector3($x, $y + $i, $z));
+                                    if ($this->message === true) {
+                                        $p->sendMessage("エレベーターを使用して" . $i . "ブロック上がりました");
+                                    }
+                                    break;
+                                }
                             }
                         }
                     }
@@ -94,18 +75,61 @@ class main extends PluginBase implements Listener
         }
     }
 
-    private function elevator ($level ,$x ,$y ,$z)
+    public function onSneak(PlayerToggleSneakEvent $ev)
     {
-        $vector3 = new Vector3($x ,$y ,$z);
+        $p = $ev->getPlayer();
+        $n = $p->getName();
+        $level = $p->getlevel();
+        $x = $p->getX();
+        $y = $p->getY();
+        $z = $p->getZ();
 
-        if(self::elevator == $level->getBlock(new Vector3($x ,$y ,$z))->getId())
+        if(empty($this->sneak[$n]))
         {
-            if(self::block == $level->getBlock(new Vector3($x ,$y - 1 ,$z))->getId())
-            {
-                return true;
-            }
+            $this->sneak[$n] = true;
         }
 
-        return false;
+        if ($ev->isSneaking()) {
+            if($this->request($p)) {
+                if ($this->sneak[$n]) {
+                    foreach ($this->world as $world) {
+                        $world = $this->getServer()->getLevelByName($world);
+                        if ($level == $world) {
+                            if ($this->elevator($level, $x, $y, $z)) {
+                                for ($i = 2; $i <= 15; $i++) {
+                                    if (self::elevator == $level->getBlock(new Vector3($x, $y - $i, $z))->getId()) {
+                                        if (self::block == $level->getBlock(new Vector3($x, $y - $i - 1, $z))->getId()) {
+                                            $p->teleport(new Vector3($x, $y - $i, $z));
+                                            if ($this->message === true) {
+                                                $p->sendMessage("エレベーターを使用して" . $i . "ブロック下りました");
+                                            }
+                                            $this->sneak[$n] = false;
+                                            break;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } else {
+            $this->sneak[$n] = true;
+        }
+    }
+
+    private function request ($p)
+    {
+        if($this->authority)
+        {
+            return true;
+        }else{
+            if($p->isOp())
+            {
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 }
